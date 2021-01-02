5bee7a1479d569ad7af5d02e077ef51aa942a528
bateszi <bateszii@gmail.com>
2021-01-02 13:39:40 +0000

    Use browser native lazy loading of images Use persistent mysql connection

--- www/_lib.php
+++ www/_lib.php
@@ -8,7 +8,10 @@ function getDbConnection() {
 		return new PDO(
 			sprintf('mysql:host=%s;dbname=%s;charset=utf8', $config['db']['server'], $config['db']['dbName']),
 			$config['db']['user'],
-			$config['db']['pass']
+			$config['db']['pass'],
+			[
+				PDO::ATTR_PERSISTENT => true
+			]
 		);
 	}
 	catch (PDOException $e)
