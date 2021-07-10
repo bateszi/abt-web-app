e8a87286b479a11a1ef624e097948dbe5c49b5cd
bateszi <bateszii@gmail.com>
2021-07-10 17:28:42 +0100

    Better crack at trending anime; this time it will segment anime by the 4 seasons in the last year

--- www/trendinganime.php
+++ www/trendinganime.php
@@ -5,39 +5,81 @@ require_once '_lib.php';
 $db = getDbConnection();
 $config = getConfig();
 
-$client = new \GuzzleHttp\Client([
-	'base_uri' => $config['solrBaseUrl'],
-	'timeout' => 2.0,
-]);
-
 $year = (int)date('Y');
 
-$period = $_GET['r'] ?? "3m";
-$solrPeriod = "";
+$seasons = [
+	[
+		'label'		=> 'Last Spring',
+		'active' 	=> mktime(0, 0, 0, 4, 1, $year-1),
+		'end'		=> mktime(0, 0, 0, 6, 30, $year-1),
+	],
+	[
+		'label'		=> 'Last Summer',
+		'active' 	=> mktime(0, 0, 0, 7, 1, $year-1),
+		'end'		=> mktime(0, 0, 0, 9, 30, $year-1),
+	],
+	[
+		'label'		=> 'Last Autumn',
+		'active' 	=> mktime(0, 0, 0, 10, 1, $year-1),
+		'end'		=> mktime(0, 0, 0, 12, 31, $year-1),
+	],
+	[
+		'label'		=> 'Winter',
+		'active' 	=> mktime(0, 0, 0, 1, 1, $year),
+		'end'		=> mktime(0, 0, 0, 3, 31, $year),
+	],
+	[
+		'label'		=> 'Spring',
+		'active' 	=> mktime(0, 0, 0, 4, 1, $year),
+		'end'		=> mktime(0, 0, 0, 6, 30, $year),
+	],
+	[
+		'label'		=> 'Summer',
+		'active' 	=> mktime(0, 0, 0, 7, 1, $year),
+		'end'		=> mktime(0, 0, 0, 9, 30, $year),
+	],
+	[
+		'label'		=> 'Autumn',
+		'active' 	=> mktime(0, 0, 0, 10, 1, $year),
+		'end'		=> mktime(0, 0, 0, 12, 31, $year),
+	],
+];
 
-switch ($period) {
-	case "3m":
-		$solrPeriod = "3MONTHS";
-		$minCount = 20;
+$currentTime = strtotime('now');
+$relevantSeasons = [];
+
+foreach ($seasons as $key => $season) {
+	if ($currentTime >= $season['active'] && $currentTime <= $season['end']) {
+		$relevantSeasons[] = $season;
+		$relevantSeasons[] = $seasons[$key - 1];
+		$relevantSeasons[] = $seasons[$key - 2];
+		$relevantSeasons[] = $seasons[$key - 3];
 		break;
-	case "12m":
-		$solrPeriod = "12MONTHS";
-		$minCount = 80;
-		break;
-	default:
-		$solrPeriod = "6MONTHS";
-		$minCount = 40;
+	}
+}
+
+$requestedSeasonIndex = $_GET['s'] ?? 0;
+
+if (isset($relevantSeasons[$requestedSeasonIndex])) {
+	$requestedSeason = $relevantSeasons[$requestedSeasonIndex];
+} else {
+	$requestedSeasonIndex = 0;
+	$requestedSeason = $relevantSeasons[0];
 }
 
 $searchOptions = [
 	'query' => [
 		'facet.field' => 'post_media',
-		'facet.limit' => -1,
-		'facet.mincount' => $minCount,
+		'facet.limit' => 100,
+		'facet.mincount' => 1,
 		'facet.sort' => 'count',
 		'facet' => 'on',
 		'fl' => 'id',
-		'fq' => sprintf('post_media_start_date:[NOW-%s TO NOW+3MONTHS]', $solrPeriod),
+		'fq' => sprintf(
+			'post_media_start_date:[%s TO %s]',
+			date('Y-m-d\T00:00:00\Z', $requestedSeason['active']),
+			date('Y-m-d\T00:00:00\Z', $requestedSeason['end'])
+		),
 		'q' => '*:*',
 		'rows' => 1,
 		'sort' => 'post_pub_date_sorter desc',
@@ -45,6 +87,10 @@ $searchOptions = [
 	]
 ];
 try {
+	$client = new \GuzzleHttp\Client([
+		'base_uri' => $config['solrBaseUrl'],
+		'timeout' => 2.0,
+	]);
 	$response = $client->request('GET', '/solr/rss/select', $searchOptions);
 	$responseCode = $response->getStatusCode();
 } catch (\GuzzleHttp\Exception\ServerException $e) {
@@ -81,5 +127,6 @@ $twig = initTwig();
 
 echo $twig->render('trendinganime.twig', [
 	'relevantAnimeSeries' => $relevantAnimeSeries,
-	'period' => $period,
+	'period' => $requestedSeasonIndex,
+	'relevantSeasons' => $relevantSeasons,
 ]);
